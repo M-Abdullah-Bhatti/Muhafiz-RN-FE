2ce71b0fb89bc8564640558880853ca6dc83f0bb
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-04-28 16:03:35 +0500

    admin dashboard api integrated

--- src/screens/TotalComplaintsRecords/TotalComplaintsRecords.js
+++ src/screens/TotalComplaintsRecords/TotalComplaintsRecords.js
@@ -1,36 +1,105 @@
-import React from 'react';
-import { StyleSheet, View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faShieldAlt, faBolt, faHandHoldingMedical } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faShieldAlt,
+  faBolt,
+  faHandHoldingMedical,
+} from "@fortawesome/free-solid-svg-icons";
+import { GetAllData } from "../../axios/NetworkCalls";
+import { useFocusEffect } from "@react-navigation/native";
+import RequestLoader from "../../component/Loader/RequestLoader";
 
-const data = [
-  { 
-    department: 'POLICE Department',
+const Data = [
+  {
+    department: "Theft Department",
     total: 114,
     left: 24,
     solved: 90,
     icon: faShieldAlt,
-    color: '#007AFF'
+    color: "#007AFF",
   },
-  { 
-    department: 'ASSAULT Department',
+  {
+    department: "ASSAULT Department",
     total: 56,
     left: 6,
     solved: 50,
     icon: faBolt,
-    color: '#34C759'
+    color: "#34C759",
   },
-  { 
-    department: 'sindh rescue Department',
+  {
+    department: "Cyber crime Department",
     total: 64,
-    left: '$375 left to spend',
+    left: "$375 left to spend",
     solved: 23,
     icon: faHandHoldingMedical,
-    color: '#AF52DE'
+    color: "#AF52DE",
   },
 ];
 
-const TotalComplaintsRecords = ({navigation}) => {
+const TotalComplaintsRecords = ({ navigation }) => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState();
+  const [Error, setError] = useState(null);
+
+  const renderIcon = (category) => {
+    console.log("category: ", category);
+    if (category == "Theft") {
+      return faShieldAlt;
+    }
+    if (category == "Assault") {
+      return faBolt;
+    }
+    return faHandHoldingMedical;
+  };
+
+  const renderIconWithColor = (category) => {
+    if (category == "Theft") {
+      return "#007AFF";
+    }
+    if (category == "Assault") {
+      return "#34C759";
+    }
+    return "#AF52DE";
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const getData = async () => {
+        try {
+          setLoading(true);
+          //   const response = await GetAllData(
+          //     `/incident/getAllIncidents?category=${category}&resolved=${false}`
+          //   );
+
+          //   ------------------------
+          const response = await GetAllData(`/incident/incidentDashboard`);
+          console.log("response: ", response);
+          if (response.success) {
+            console.log("response:  ", response);
+            setData(response?.data);
+          } else {
+            // console.log("errorrr: ", response);
+            setError(response.message);
+          }
+        } catch (err) {
+          console.error("Error fetching data:", err);
+          setError(err.message); // Use err.message for consistency
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      getData(); // Execute the function instead of returning it
+    }, []) // Include currentPage in the dependency array
+  );
   return (
     <View style={styles.container}>
       {/* Header */}
@@ -39,31 +108,58 @@ const TotalComplaintsRecords = ({navigation}) => {
       {/* Gauge Chart Placeholder */}
       <View style={styles.chartContainer}>
         <Image
-          source={require('../../../src/assets/images/chart.png')}
-          style={styles.profileImage} 
+          source={require("../../../src/assets/images/chart.png")}
+          style={styles.profileImage}
         />
-        <Text style={styles.chartText}>Total 234 Complaints</Text>
+        <Text style={styles.chartText}>
+          Total ${Error ? 0 : data?.length} Complaints
+        </Text>
       </View>
 
       {/* Complaint List */}
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.department}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.listItem}
-            onPress={() => {navigation.navigate('SolvedComplaints', {department: item.department})}}
-          >
-            <FontAwesomeIcon icon={item.icon} size={24} style={{ color: item.color }} />
-            <View style={styles.textContainer}>
-              <Text style={styles.departmentName}>{item.department}</Text>
-              <Text style={styles.complaintText}>{item.total} Total</Text>
-              <Text style={styles.leftToSolveText}>{item.left} left to solve</Text>
-              <Text style={styles.solvedText}>{item.solved} Solved</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      />
+      {loading ? (
+        <RequestLoader size="large" color="yellow" />
+      ) : Error ? (
+        <Text
+          style={{
+            textAlign: "center",
+            color: "white",
+            marginTop: 20,
+            fontSize: 16,
+          }}
+        >
+          {Error}
+        </Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.department}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.listItem}
+              onPress={() => {
+                navigation.navigate("SolvedComplaints", {
+                  category: item.department,
+                });
+              }}
+            >
+              <FontAwesomeIcon
+                icon={renderIcon(item.category)}
+                size={24}
+                style={{ color: renderIconWithColor(item.category) }}
+              />
+              <View style={styles.textContainer}>
+                <Text style={styles.departmentName}>{item?.category}</Text>
+                <Text style={styles.complaintText}>{item.total} Total</Text>
+                <Text style={styles.leftToSolveText}>
+                  {item.left} left to solve
+                </Text>
+                <Text style={styles.solvedText}>{item.solved} Solved</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      )}
     </View>
   );
 };
@@ -71,17 +167,17 @@ const TotalComplaintsRecords = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: "#000",
   },
   header: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     marginVertical: 20,
     marginTop: 60,
   },
   chartContainer: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   profileImage: {
     width: 200,
@@ -89,35 +185,35 @@ const styles = StyleSheet.create({
     marginBottom: 80,
   },
   listItem: {
-    flexDirection: 'row',
-    backgroundColor: '#333',
+    flexDirection: "row",
+    backgroundColor: "#333",
     padding: 20,
     borderRadius: 10,
     marginVertical: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   textContainer: {
     marginLeft: 10,
   },
   departmentName: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   complaintText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
   },
   chartText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
   },
   leftToSolveText: {
-    color: '#FF3B30',
+    color: "#FF3B30",
     fontSize: 16,
   },
   solvedText: {
-    color: '#34C759',
+    color: "#34C759",
     fontSize: 16,
   },
 });
